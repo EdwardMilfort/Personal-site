439c1f4e50887b85bfb1d3ab83c84ae1423b427d
Edward Milfort <edwardmilfort@gmail.com>
2024-07-10 15:06:03 -0400

    made

--- 00-intro/intro.js
+++ 00-intro/intro.js
@@ -8,6 +8,7 @@ const colorField = document.getElementById("color-field");
 const fontSizeField = document.getElementById("font-size-fied");
 const clickButton = document.getElementById("click-button");
 const text = document.getElementById("text");
+const checkbox = document.getElementById("checkbox")
 const body = document.body;
 
 function buttonPress() {
@@ -16,4 +17,14 @@ function buttonPress() {
     body.style.backgroundColor = colorField.value;
     text.style.fontSize = `${fontSizeField.value}px`;
     let fail = '${fontSizeField.value}px';
+}
+function checkboxChange() {
+    let checked = checkbox.checked;
+    console.log(`the checkbox has this state: ${checked}`);
+
+    if (checked) {
+        body.style.outlineStyle = "solid";
+    } else {
+        body.style.outlineStyle = "none";
+    }
 }
