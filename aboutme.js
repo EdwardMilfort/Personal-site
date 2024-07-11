39c38d4ac0526bb42293cda84b8c9a9d195ca135
Edward Milfort <edwardmilfort@gmail.com>
2024-07-11 08:30:00 -0400

    made

---
+++ aboutme.js
@@ -0,0 +1,24 @@
+const colorField = document.getElementById("color-field"); 
+const fontSizeField = document.getElementById("font-size-fied");
+const clickButton = document.getElementById("click-button");
+const text = document.getElementById("text");
+const checkbox = document.getElementById("checkbox")
+const body = document.body;
+
+function buttonPress() {
+    text.innerHTML = "BANANANANANANANANANANANANANA";
+
+    body.style.backgroundColor = colorField.value;
+    text.style.fontSize = `${fontSizeField.value}px`;
+    let fail = '${fontSizeField.value}px';
+}
+function checkboxChange() {
+    let checked = checkbox.checked;
+    console.log(`the checkbox has this state: ${checked}`);
+
+    if (checked) {
+        body.style.outlineStyle = "solid";
+    } else {
+        body.style.outlineStyle = "none";
+    }
+}
