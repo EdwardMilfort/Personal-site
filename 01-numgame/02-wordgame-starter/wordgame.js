37db6d4d627199ca530159e5c3515606156be5ef
Edward Milfort <edwardmilfort@gmail.com>
2024-07-12 10:19:44 -0400

    made

--- 01-numgame/02-wordgame-starter/wordgame.js
+++ 01-numgame/02-wordgame-starter/wordgame.js
@@ -37,20 +37,53 @@ function randomBackgroundColor() {
 // For array of words: let arr = Object.keys(json)
 // For a random word:  let word = arr[randInt(0, arr.length - 1)];
 const randomWord = document.getElementById("random-word");
+const guessField = document.getElementById("guess-field");
+const feedbackText = document.getElementById("feedback-text");
 let allWords = [];
-let fiveLetterWords = []
+let fiveLetterWords = [];
+let secret = '';
 function wordsLoaded() {
-    allWords = Object.keys(json)
+    allWords = Object.keys(json);
     let randomIndex = randInt(0, allWords.length-1);
-    randomWord.innerHtml = allWords[randomIndex]
+    randomWord.innerHTML = allWords[randomIndex];
 
 
     for (let i = 0; i < allWords.length; i++) {
-        let word = allwords[i];
-        if (word.length != 5) continue
+        let word = allWords[i];
+        if (word.length != 5) continue;
         fiveLetterWords.push(allWords[i]);
     }
     
     randomIndex = randInt(0, fiveLetterWords.length-1);
-    secret = fiveLetterWords
+    secret = fiveLetterWords[randomIndex];
+}
+
+function changeGuess() {
+    let guess = guessField.value.toLowerCase();
+
+
+    if (guess.length < 5) return;
+
+    if (guess.length > 5) {
+        guessField.value = "";
+        return;
+    }
+    console.log(`guess: "${guess}"`);
+
+
+    if (!json.hasOwnProperty(guess)) {
+        console.log(`this IS  NOT a word!`);
+    feedbackText.innerHTML += `"${guess}" is not a word. Try again.<br>`
+    guessField.value = "";
+    return;
+    }
+    
+    let correctPlacement = 0;
+    for (let i = 0; i< 5; i++) {
+        if(guess [i] == secret [i]) {
+            correctPlacement++;
+        }
+    }
+    feedbackText.innerHTML += `<span class ="correct"> ${guess}" </span> has ${correctPlacement} letter(s) in the correct place`
+    guessField.value = "";
 }
